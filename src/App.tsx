421e79a431f3dbcb8345042a60701ca028ac4ad3
jhonata velandia <jhonatav@localhost.localdomain>
2024-09-11 22:18:32 -0500

    primer comit react

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+//import React from "react";
+import "./App.css";
+//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "datatables.net-dt/css/jquery.dataTables.css";
+//import LoginForm from "./components/LoginForm/LoginForm";
+//import HomePage from "./components/HomePage/HomePage";
+//import TablaClientes from "./components/tablaClientes/tablaClientes"; // Importa el componente correctamente
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
